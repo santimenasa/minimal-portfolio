c226eee4c40fec19e023b95e98089ec70333f876
Santiago Mena <santimenasa@gmail.com>
2023-01-24 12:36:37 -0300

    complete timeline experience

--- src/components/data/timeline.js
+++ src/components/data/timeline.js
@@ -1,20 +1,20 @@
 export default [
     {
         year:'2020',
-        title:'ministerio de ambiente y desarrollo sostenible',
-        duration:'too many years',
-        detail:'Working with a API to create the fron end of the web page',
+        title:'Movistar',
+        duration:'1 Year',
+        detail:'Working with diferent APIs to create the fron end of the web page',
     },
     {
-        year:'2020',
+        year:'2022',
         title:'Bird finder San isidro',
-        duration:'too many years',
+        duration:'1 Week',
         detail:'Made a web app to keep track every bird species found in san isidro',
     },
     {
-        year:'2020',
+        year:'2022',
         title:'weather app',
-        duration:'too many years',
-        detail:'i made a weather app from a existing api to check the weather in all the world',
+        duration:'3 Days',
+        detail:'I made a weather app from an existing api to check the weather in all the world',
     }
 ]
